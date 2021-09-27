52e3900c533922d611c03d76f4bbb825075f68e0
Vincent Han <vincent@acepod.com>
2021-09-27 10:30:15 +0800

    Create k8s.sh

---
+++ scripts/k8s.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+alias k=kubectl
+source <(kubectl completion bash)
+complete -F __start_kubectl k
+echo 'alias k=kubectl' >> ~/.bashrc
+echo ' alias kga='kubectl get all' >> ~/.bashrc
+echo 'alias kaf='kubectl apply -f' >> ~/.bashrc
+echo 'alias kd='kubectl describe' >> ~/.bashrc
+source .bashrc
+echo 'set nu ts=2 sw=2 expandtab ruler' > ~/.vimrc
+echo 'set backspace=indent,eol,start' >> ~/.vimrc
+source ~/.vimrc
+export do="--dry-run=client -oyaml"
+export now="--grace-period=0 --force"
