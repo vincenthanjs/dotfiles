7297dc83937a839284d0798989e41b6dc3a6778a
Vincent Han <vincent@acepod.com>
2021-09-27 11:33:33 +0800

    Update k8s.sh

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -3,9 +3,9 @@ alias k=kubectl
 source <(kubectl completion bash)
 complete -F __start_kubectl k
 echo 'alias k=kubectl' >> ~/.bashrc
-echo 'alias kga='kubectl get all' >> ~/.bashrc
-echo 'alias kaf='kubectl apply -f' >> ~/.bashrc
-echo 'alias kd='kubectl describe' >> ~/.bashrc
+echo 'alias kga=kubectl get all' >> ~/.bashrc
+echo 'alias kaf=kubectl apply -f' >> ~/.bashrc
+echo 'alias kd=kubectl describe' >> ~/.bashrc
 source ~/.bashrc
 echo 'set nu ts=2 sw=2 expandtab ruler' > ~/.vimrc
 echo 'set backspace=indent,eol,start' >> ~/.vimrc
