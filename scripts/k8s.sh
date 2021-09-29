24e6154754d8b86d68d7bb437d4c03968de794b1
vincenthanjs <vincent@acepod.com>
2021-09-30 09:39:35 +1000

    update k8s delete and describe

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -3,11 +3,13 @@ alias k=kubectl
 source <(kubectl completion bash)
 complete -F __start_kubectl k
 echo "alias k='kubectl'" >> ~/.bashrc
+echo "alias ke'kubectl exec -it'" >> ~/.bashrc
 echo "alias kg='kubectl get'" >> ~/.bashrc
 echo "alias kga='kubectl get all'" >> ~/.bashrc
 echo "alias kaf='kubectl apply -f'" >> ~/.bashrc
 echo "alias kdf='kubectl delete -f'" >> ~/.bashrc
-echo "alias kd='kubectl describe'" >> ~/.bashrc
+echo "alias kd='kubectl delete'" >> ~/.bashrc
+echo "alias kdc='kubectl describe'" >> ~/.bashrc
 echo "export do='--dry-run=client -oyaml'" >> ~/.bashrc
 echo "export now='--grace-period=0 --force'" >> ~/.bashrc
 source ~/.bashrc
